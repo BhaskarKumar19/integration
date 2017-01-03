bead605fc138c4c1304241a4b1c1b1589847b30f
Bhaskar <nickybhaskar@gmail.com>
2017-01-03 23:25:24 +0530

    COMMIT ISSUE
    committing TntgrnConstants again. Also commiting unsaved changes in xml
    file.

---
+++ springIntegration/src/main/java/integration/IntgrnContants.java
@@ -0,0 +1,16 @@
+package integration;
+
+public class IntgrnContants {
+
+	//public static final String OUTBOUND_Url="";
+	//public static final String INBOUND_URL="";
+	
+	public static final String OUTBOUND_URL="tcp://iot.eclipse.org:1883";
+	public static final String INBOUND_URL="tcp://iot.eclipse.org:1883";
+	
+	
+	//public static final String OUTBOUND_Url="tcp://192.168.0.102:1883";
+	//public static final String INBOUND_URL="tcp://192.168.0.102:1883";
+	
+	
+}
