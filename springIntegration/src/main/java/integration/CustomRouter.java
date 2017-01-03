e86a6e4606f1687120f0e475373b9fbc6887dcbc
Bhaskar <nickybhaskar@gmail.com>
2017-01-03 23:18:49 +0530

    BUG: mqtt was not publishing data
    Output channel of gateway service was pointing to router only.
    
    Added constant to store broker url

--- springIntegration/src/main/java/integration/CustomRouter.java
+++ springIntegration/src/main/java/integration/CustomRouter.java
@@ -6,7 +6,7 @@ import org.springframework.messaging.Message;
 public class CustomRouter {
 
 	public String route(Message<?> arg0) {
-		System.out.println("Router invoked");
+		System.out.println("####################Router invoked#####################");
 		String topic = (String) arg0.getHeaders().get(MqttHeaders.TOPIC);
 
 		if (topic != null && topic.equalsIgnoreCase("test2/integrationTest/SENSOR")) {
--- springIntegration/src/main/java/integration/Main.java
+++ springIntegration/src/main/java/integration/Main.java
@@ -2,13 +2,16 @@ package integration;
 
 import java.util.concurrent.Future;
 
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.support.MqttHeaders;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
 
 public class Main {
 
@@ -34,9 +37,22 @@ public class Main {
 			}
 		}    */
 		
+		
+		
+		MqttPahoMessageDrivenChannelAdapter messageChannelAdaptor;
+		messageChannelAdaptor=context.getBean("mqttInboundAdapter",MqttPahoMessageDrivenChannelAdapter.class);
+		messageChannelAdaptor.addTopic("test2/integrationTest/SENSOR");
+		
 		CustomGateway gatewayService;
 		gatewayService= context.getBean("customGatewayService", CustomGateway.class);
-		Future resp=gatewayService.sendMqttMessage(MessageBuilder.withPayload("MQTT TEST PAYLOAD!").build());
+		
+		
+		@SuppressWarnings("rawtypes")
+		Message m= MessageBuilder.withPayload("DYNAMIC PAYLOAD!")
+				.setHeader(MqttHeaders.TOPIC, "test2/integrationTest/SENSOR").build();
+		
+		@SuppressWarnings("rawtypes")
+		Future resp=gatewayService.sendMqttMessage(m);
 		
 		while (!resp.isDone()) {
 			System.out.println("Task is not completed yet....");
@@ -44,7 +60,7 @@ public class Main {
 		}
 		System.out.println("Task is completed !!.");
 		System.out.println(resp.get());
-		
+			
 		context.close();
 	}
 
